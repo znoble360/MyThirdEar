1e87c7275bb1a839f51d1d1c838c5037f1cb34ad
Nelson Torres <nelsonjtsm@gmail.com>
2021-08-26 16:32:20 -0400

    test(player): remove widget_test from default code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:musictranscriptiontools/main.dart';
+// import 'package:musictranscriptiontools/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    /*
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
@@ -26,5 +27,6 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+    */
   });
 }
