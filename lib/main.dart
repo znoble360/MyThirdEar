33645106299baeed9a0d6e1eb26d353371517b3d
Nelson Torres <nelsonjtsm@gmail.com>
2021-10-17 15:28:14 -0400

    feat(file): create directory with hash name
    - Creates a new directory for every audio file the user uploads
    - If the user has already uploaded the file,
    it doesn't generate the directory again

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   late AudioPlayer _player;
   GlobalKey<ScaffoldState> _globalKey = GlobalKey();
-  String _outputPath = "";
+  late Directory _appDocDir;
 
   @override
   void initState() {
@@ -38,12 +38,7 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
 
   Future<void> _init() async {
     // Open Documents Directory
-    Directory appDocumentDir = await getApplicationDocumentsDirectory();
-    String rawDocumentPath = appDocumentDir.path;
-
-    _outputPath = rawDocumentPath;
-
-    // TODO: To write an mp3, get the path first and then write as bytes as shown here: https://pub.dev/documentation/mp3editor/latest/
+    _appDocDir = await getApplicationDocumentsDirectory();
 
     final session = await AudioSession.instance;
     await session.configure(AudioSessionConfiguration.speech());
@@ -87,7 +82,7 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
         key: _globalKey,
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            selectFileForPlayer(_player, _outputPath);
+            selectFileForPlayer(_player, _appDocDir);
           },
           child: const Icon(Icons.file_upload),
           backgroundColor: Colors.yellowAccent,
--- lib/utils/file_handler.dart
+++ lib/utils/file_handler.dart
@@ -1,40 +1,69 @@
+import 'dart:io';
+
+import 'package:crypto/crypto.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
 import 'package:just_audio/just_audio.dart';
 
 // Try to load audio from a source and catch any errors.
-void selectFileForPlayer(AudioPlayer player, String savePath) async {
+void selectFileForPlayer(AudioPlayer player, Directory appDocDir) async {
   try {
-    String inPath = ""; // the input file path on-device
-
     // Call to open file manager on android and iOS. Choose only one file for now.
-    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      withData: true,
+    );
 
-    if (result != null) {
-      PlatformFile file = result.files.first;
-
-      inPath = file.path; // get the cached file
-    } else {
+    if (result == null) {
       // User did not select a file, don't do anything.
       return;
     }
 
-    // Replace extension for savePath if not an mp3 file
-    String ext = inPath.substring(inPath.lastIndexOf('.'));
-    savePath = inPath.replaceAll(ext, ".mp3");
+    PlatformFile file = result.files.first;
+
+    String md5Hash = _generateMD5Hash(file);
+    final Directory newDirectory = Directory('${appDocDir.path}/$md5Hash');
+    if (await newDirectory.exists()) {
+      // Directory already exists, meaning that we already dealt with this song before.
+      // TODO: Check that the files we expect to have exist.
+      String audioMP3Path = '${newDirectory.path}/audio.mp3';
+
+      print("We've already seen this file, look at cached files");
+
+      // Set the audio source given file input path
+      await player.setAudioSource(AudioSource.uri(Uri.file(audioMP3Path)),
+          initialPosition: Duration.zero, preload: true);
+
+      return;
+    }
+
+    // Create directory since it doesn't exist yet.
+    final Directory finalDirectory = await newDirectory.create(recursive: true);
+    final String dirPath = finalDirectory.path;
+
+    String audioMP3Path = '$dirPath/audio.mp3';
+    // TODO: Generate waveform data and save it to waveform.bin
+    // String waveformBinPath = '$dirPath/waveform.bin';
 
     // Run FFmpeg on this single file and store it in app data folder
     final FlutterFFmpeg flutterFFmpeg = new FlutterFFmpeg();
-    String commandToExecute = '-i ' + "\'"+ inPath + "\'" + " " + "\'"+savePath+"\'";
-    print("command to execute" + commandToExecute);
+    String convertToMp3Command = '-i ${file.path} $audioMP3Path';
+
     flutterFFmpeg
-        .execute(commandToExecute)
+        .execute(convertToMp3Command)
         .then((rc) => print("FFmpeg process exited with rc $rc"));
 
     // Set the audio source given file input path
-    await player.setAudioSource(AudioSource.uri(Uri.file(savePath)),
+    await player.setAudioSource(AudioSource.uri(Uri.file(audioMP3Path)),
         initialPosition: Duration.zero, preload: true);
   } catch (e) {
     print("Error loading audio source: $e");
   }
+}
+
+String _generateMD5Hash(PlatformFile file) {
+  if (file.bytes == null) {
+    return "";
+  }
+
+  return md5.convert(file.bytes!).toString();
 }
