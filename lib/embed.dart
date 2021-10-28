3b6d38bf6fec1eb07687653f4e8c6df9b229d1b7
SiyuanTang1001 <siyuantang193@yahoo.com>
2021-10-27 20:25:17 -0400

    Add new UI: library main screen, updated music player screen. Does not have filepicker implementation.

---
+++ lib/embed.dart
@@ -0,0 +1,3 @@
+export 'package:musictranscriptiontools/ui/common/piano_section.dart';
+export 'package:musictranscriptiontools/plugins/midi/midi.dart';
+export 'package:musictranscriptiontools/ui/theme.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,237 +1,101 @@
-import 'package:audio_session/audio_session.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:just_audio/just_audio.dart';
-import 'package:musictranscriptiontools/cards/pitch.dart';
-import 'package:musictranscriptiontools/cards/speed.dart';
-import 'package:musictranscriptiontools/utils/file_handler.dart';
-import 'package:rxdart/rxdart.dart';
-import 'dart:async';
-import 'package:path_provider/path_provider.dart';
-import 'dart:io';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:musictranscriptiontools/Home.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'utils/common.dart';
+import 'package:musictranscriptiontools/data/blocs/blocs.dart';
+import 'package:musictranscriptiontools/ui/theme.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
-  late AudioPlayer _player;
-  GlobalKey<ScaffoldState> _globalKey = GlobalKey();
-  String _outputPath = "";
+class _MyAppState extends State<MyApp> {
+  final _settingsBloc = SettingsBloc();
 
   @override
   void initState() {
+    _settingsBloc.add(CheckSettings());
     super.initState();
-    WidgetsBinding.instance?.addObserver(this);
-    _player = AudioPlayer();
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: Colors.black,
-    ));
-    _init();
-  }
-
-  Future<void> _init() async {
-    // Open Documents Directory
-    Directory appDocumentDir = await getApplicationDocumentsDirectory();
-    String rawDocumentPath = appDocumentDir.path;
-
-    _outputPath = rawDocumentPath;
-
-    // TODO: To write an mp3, get the path first and then write as bytes as shown here: https://pub.dev/documentation/mp3editor/latest/
-
-    final session = await AudioSession.instance;
-    await session.configure(AudioSessionConfiguration.speech());
-    // Listen to errors during playback.
-    _player.playbackEventStream.listen((event) {},
-        onError: (Object e, StackTrace stackTrace) {
-      print('A stream error occurred: $e');
-    });
   }
 
   @override
   void dispose() {
-    WidgetsBinding.instance?.removeObserver(this);
-    _player.dispose();
+    _settingsBloc.close();
     super.dispose();
   }
 
   @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.paused) {
-      // Release the player's resources when not in use. We use "stop" so that
-      // if the app resumes later, it will still remember what position to
-      // resume from.
-      _player.stop();
-    }
-  }
-
-  Stream<PositionData> get _positionDataStream =>
-      Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
-          _player.positionStream,
-          _player.bufferedPositionStream,
-          _player.durationStream,
-          (position, bufferedPosition, duration) => PositionData(
-              position, bufferedPosition, duration ?? Duration.zero));
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        key: _globalKey,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            selectFileForPlayer(_player, _outputPath);
-          },
-          child: const Icon(Icons.file_upload),
-          backgroundColor: Colors.yellowAccent,
-        ),
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              GestureDetector(
-                  onTap: () {
-                    _globalKey.currentState?.openDrawer();
+    final theme = ThemeUtils(context);
+    return ScreenUtilInit(
+        designSize: Size(750, 1624),
+        builder: () => MultiBlocProvider(
+              providers: [
+                BlocProvider<SettingsBloc>(create: (_) => _settingsBloc),
+              ],
+                child: BlocBuilder<SettingsBloc, SettingsState>(
+                  builder: (context, settingState) {
+                    ThemeMode mode = ThemeMode.system;
+                    if (settingState is SettingsReady) {
+                      mode = settingState.themeMode;
+                    }
+                    return MaterialApp(
+                      debugShowCheckedModeBanner: false,
+                      theme: theme.light,
+                      themeMode: mode,
+                      home: Home(),
+                      onGenerateTitle: (context) => "My Third Ear",
+                      locale: Locale("en", "US"),
+                    );
                   },
-                  child: Container(
-                    padding: EdgeInsets.all(15),
-                    child: Icon(Icons.dehaze, color: Colors.grey),
-                  )),
-              SizedBox(height: 8.0),
-              Row(
-                children: [],
-              ),
-              SizedBox(height: 400),
-              StreamBuilder<PositionData>(
-                stream: _positionDataStream,
-                builder: (context, snapshot) {
-                  final positionData = snapshot.data;
-                  return SeekBar(
-                    duration: positionData?.duration ?? Duration.zero,
-                    position: positionData?.position ?? Duration.zero,
-                    bufferedPosition:
-                        positionData?.bufferedPosition ?? Duration.zero,
-                    onChangeEnd: (newPosition) {
-                      _player.seek(newPosition);
-                    },
-                  );
-                },
-              ),
-              ControlButtons(_player),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ControlButtons extends StatelessWidget {
-  final AudioPlayer player;
-
-  ControlButtons(this.player);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              width: 120,
-              child: Row(
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.volume_up),
-                    onPressed: () {
-                      showSliderDialog(
-                        context: context,
-                        title: "Adjust volume",
-                        divisions: 10,
-                        min: 0.0,
-                        max: 1.0,
-                        value: player.volume,
-                        stream: player.volumeStream,
-                        onChanged: player.setVolume,
-                      );
-                    },
-                  ),
-                  StreamBuilder<SequenceState?>(
-                    stream: player.sequenceStateStream,
-                    builder: (context, snapshot) => IconButton(
-                      icon: Icon(Icons.skip_previous),
-                      onPressed:
-                          player.hasPrevious ? player.seekToPrevious : null,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            StreamBuilder<PlayerState>(
-              stream: player.playerStateStream,
-              builder: (context, snapshot) {
-                final playerState = snapshot.data;
-                final processingState = playerState?.processingState;
-                final playing = playerState?.playing;
-                if (processingState == ProcessingState.loading ||
-                    processingState == ProcessingState.buffering) {
-                  return Container(
-                    margin: EdgeInsets.all(8.0),
-                    // width: 64.0,
-                    // height: 64.0,
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (playing != true) {
-                  return IconButton(
-                    icon: Icon(Icons.play_arrow),
-                    //iconSize: 64.0,
-                    onPressed: player.play,
-                  );
-                } else if (processingState != ProcessingState.completed) {
-                  return IconButton(
-                    icon: Icon(Icons.pause),
-                    //iconSize: 64.0,
-                    onPressed: player.pause,
-                  );
-                } else {
-                  return IconButton(
-                    icon: Icon(Icons.replay),
-                    //iconSize: 64.0,
-                    onPressed: () => player.seek(Duration.zero,
-                        index: player.effectiveIndices!.first),
-                  );
-                }
-              },
-            ),
-            Container(
-              width: 100,
-              child: StreamBuilder<SequenceState?>(
-                stream: player.sequenceStateStream,
-                builder: (context, snapshot) => IconButton(
-                  icon: Icon(Icons.skip_next),
-                  onPressed: player.hasNext ? player.seekToNext : null,
                 ),
-              ),
-            ),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SpeedCard(player),
-            PitchCard(player),
-          ],
-        ),
-      ],
-    );
+            ));
   }
 }
+
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({
+//     Key? key,
+//   }) : super(key: key);
+
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+
+//   @override
+//   void dispose() {
+//     super.dispose();
+//   }
+
+//   @override
+//     Widget build(BuildContext context) {
+//     final theme = ThemeUtils(context);
+//     return ScreenUtilInit(
+//       designSize: Size(750, 1624),
+//       builder: () {
+//         return MaterialApp(
+//           theme: theme.light,
+//           home: Home(),
+//         );
+//       },
+//     );
+//   }
+// }
